aff954806126d18f16c879d4b272a0f61ff48df3
Vitor Zanela <vitor.mardig@gmail.com>
2025-06-23 02:36:32 -0300

    Criado a classe menu ja com algumas funções de interação

---
+++ classes/Menu.rb
@@ -0,0 +1,37 @@
+class Menu
+    def escolherOpc()
+        puts "1 - Busca por CEP / 2 - Busca por endereço"
+        opc = gets.chomp.to_i
+        return opc
+    end
+
+    def lerCep()
+        puts "Digite o CEP (Somente números)"
+        cep = gets.chomp.to_i
+        return cep
+    end
+
+    def buscaEndereco()
+        puts "Digite o Estado (EX: SP, MG, BA...)"
+        estado = gets.chomp
+        puts "Digite a cidade (EX: São Paulo, Porto Seguro, Rio de Janeiro...)"
+        cidade = gets.chomp
+        puts "Digite o nome da Rua(Rua, Avenida, Estrada ...)"
+        rua = gets.chomp
+
+        return [estado, cidade, rua]
+    end
+
+    def desejaContinuar()
+        puts "Deseja continuar? (S/N)"
+        resp = gets.chomp.downcase
+        if resp == "s"
+            true
+        elsif resp == "n"
+            false
+        else
+            puts "Resposta Incorreta! Tente novamente!"
+        end
+    end
+
+end
