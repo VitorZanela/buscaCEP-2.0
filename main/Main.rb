6fb01a6fad394f07b32655983683a8eb1634cedf
Vitor Zanela <vitor.mardig@gmail.com>
2025-06-23 17:15:12 -0300

    Incluido a requisição da API baseado no CEP

--- main/Main.rb
+++ main/Main.rb
@@ -1,14 +1,28 @@
 require_relative "../classes/Menu"
+require_relative "../classes/ServiceBuscaCep"
+require_relative "../classes/BuscaCep"
 
 puts "---- BUSCA DE ENDEREÇO ----"
 
 menu = Menu.new
+
+
+
+
 opc = menu.escolherOpc()
 if opc == 1
     buscaCep = menu.lerCep()
-    if buscaCep.to_s.length != 8
+    while buscaCep.to_s.length != 8 do ##Corrigir erro de validação, quando erra esta puts pra sempre
         puts "Verifique o CEP e tente novamente!"
     end
+
+    resultado = BuscaCep.new
+    cepService = ServiceBuscaCep.new
+
+    cepService.buscar(buscaCep)
+
+    puts cepService
+
 else
     puts "estou na 2"
 end
