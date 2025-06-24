9c44d31677aa4708e07ac50dec8aca7fe987dbb5
Vitor Zanela <vitor.mardig@gmail.com>
2025-06-24 00:30:25 -0300

    Inicio da inclusão da CPI para busca não demorar

--- main/Main.rb
+++ main/Main.rb
@@ -1,6 +1,8 @@
 require_relative "../classes/Menu"
 require_relative "../classes/ServiceBuscaCep"
+require_relative "../classes/ServiceBuscaEnd"
 require_relative "../classes/BuscaCep"
+
 require 'json'
 
 puts "---- BUSCA DE ENDEREÇO ----"
@@ -28,5 +30,18 @@ if opc == 1
     puts "---------------------"
 
 else
-    puts "estou na 2"
+
+    dados = menu.buscaEndereco()
+
+
+    endService = ServiceBuscaEnd.new
+    listaDados = dados[0],dados[1],dados[2]
+    resultado = endService.buscar(listaDados)
+    enderecosList = JSON.parse(resultado, symbolize_names: true)
+    
+    
+    resulEditado = BuscaCep.from_api(enderecosList.first) #Ainda apresenta erros
+    puts "----- RESULTADO -----"
+    puts resulEditado
+    puts "---------------------"
 end
