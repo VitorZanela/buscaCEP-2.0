fdc1672e49f9d971cab632133c671e859611835e
Vitor Zanela <vitor.mardig@gmail.com>
2025-06-25 00:22:13 -0300

    Incluso begin/rescue para tratamento de exceções

--- classes/ServiceBuscaCep.rb
+++ classes/ServiceBuscaCep.rb
@@ -3,8 +3,18 @@ require 'uri'
 
 class ServiceBuscaCep
     def buscar(cep)
-        uri = URI("https://viacep.com.br/ws/#{cep}/json/")
-        response = Net::HTTP.get_response(uri)
-        return response.body
+        begin
+            uri = URI("https://viacep.com.br/ws/#{cep}/json/")
+            response = Net::HTTP.get_response(uri)
+            if response.code == "400"
+                return nil
+            elsif response.body.nil? || response.body.include?("erro")
+                return nil
+            end
+            return response.body
+
+        rescue StandardError => e
+            return nil
+        end
     end
 end 
