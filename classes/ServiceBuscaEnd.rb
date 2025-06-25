e6bdf4d4acf6ac89b6aa37976a40793f2da750d9
Vitor Zanela <vitor.mardig@gmail.com>
2025-06-25 01:35:29 -0300

    Incluso begin/rescue para tratamento de exceções

--- classes/ServiceBuscaEnd.rb
+++ classes/ServiceBuscaEnd.rb
@@ -3,9 +3,16 @@ require 'uri'
 
 class ServiceBuscaEnd
     def buscar(estado, cidade, rua)
+      begin
         uri = URI("https://viacep.com.br/ws/#{estado}/#{formatar(cidade)}/#{formatar(rua)}/json/")
         response = Net::HTTP.get_response(uri)
+        if response.code == "400" 
+          return nil
+        end
         return response.body
+        rescue StandardError => e
+            return nil
+        end
     end
 
     private 
